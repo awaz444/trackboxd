fe1df484d964276659a21f1a75ab98ed11336530
Aawaiz <aawaizali8@gmail.com>
2025-09-21 17:11:28 +0500

    for font

--- trackboxd-new/src/app/layout.tsx
+++ trackboxd-new/src/app/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from "next";
 import { Lora } from "next/font/google";
 import "./globals.css";
 
-const merriweather = Lora({ subsets: ["latin"], weight: ["400", "700"] });
+const lora = Lora({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export const metadata: Metadata = {
   title: "Trackboxd",
@@ -11,12 +14,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={`${merriweather.className} bg-[#FFFFF0] text-[#1F2C24]`}>
+      <body className={`${lora.className} bg-[#FFFFF0] text-[#1F2C24]`}>
         {children}
       </body>
     </html>
