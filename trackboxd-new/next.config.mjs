4c6f30d8ad14f9a29ff31db2f1044ef26c837f85
Aawaiz <aawaizali8@gmail.com>
2026-05-16 13:48:08 +0500

    revery next js config

--- trackboxd-new/next.config.mjs
+++ trackboxd-new/next.config.mjs
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-            {
-                source: '/:path*',
-                has: [{ type: 'host', value: 'www.trackboxd.com' }],
-                destination: 'https://trackboxd.com/:path*',
-                permanent: true,
-            },
-        ];
-    },
     async headers() {
         return [
             {
